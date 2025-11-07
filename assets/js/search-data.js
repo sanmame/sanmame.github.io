5a81c38765cd96e694a6563f4c91bdec7b421114
sanmame <sanmame@gmail.com>
2025-11-07 13:40:18 +0000

    Deploying to gh-pages from @ sanmame/sanmame.github.io@f7af819a7679031bd8c4d67e32d3440e519699e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "Participation in funded research projects",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",

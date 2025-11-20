3c5bb1e73f6a5d0d44f949f05ec9c697b53e17a8
sanmame <sanmame@gmail.com>
2025-11-20 14:52:36 +0000

    Deploying to gh-pages from @ sanmame/sanmame.github.io@dd295b077d95e27f164f1548d03fbd693783beca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
